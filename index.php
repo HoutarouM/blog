184a88c6c89c65f73aa0b97fbf4ef4949adec907
HoutarouM <maryan.goroshko@ukr.net>
2022-03-14 13:23:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+    TODO:
+    - Logowanie
+        - sesja
+    - Rejestracja
+        - moc hasla 
+        - hashowanie
+    - Dodawanie postow - tylko dla zalogowanych
+    - Wyswetlanie postow - dla wszystkich
+    - komentowanie
+    - hierarcjia
+
+    Baza danych:
+    - nazwa forum
+    tabele:
+    - Posty
+        - id 
+        - tytul
+        - kategorie
+        - id_autora
+        - postnadzendny
+    - Users
+        - id
+        - email
+        - haslo 
+        - nick
+    - Polubienia
+        - id
+        - id_uzytkownika
+        - id_posta
+
+    - dane do bazy w osobnym pliku 
+*/
+?>
