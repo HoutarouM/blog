add35347f4b032ee5d84919e3dadb00b93937ee3
HoutarouM <maryan.goroshko@ukr.net>
2022-03-29 18:42:11 +0200

    Merge branch 'main' of https://github.com/HoutarouM/forum_study

--- view/posts.php
+++ view/posts.php
@@ -1,37 +1,12 @@
 <?php include_once './view/assets/header.php'; ?>
 
 <div class="main-content">
-    <h2>Last Discussions</h2>
-
-    <div class="discussions-and-nav-wrapper">
-        <div class="discussions">
-
-            <?php $posts_data = $this->getPostsData($data);
-
-            if (!$posts_data) {
-                echo "No posts";
-            }
-
-            foreach ($posts_data as $post_data) : ?>
-
-                <div class="discussion">
-
-                    <a href="?post=<?= $post_data['id'] ?>">
-                        <h2><?= $post_data['tytul'] ?></h2>
-                    </a>
-                    <div class="post-data">
-                        <p>Author: <?= $this->getAuthorData($post_data['id']) ?></p>
-                        <p>Likes: <?= $this->getLikesData($post_data['id']) ?></p>
-                    </div>
-                    <p><?= substr($post_data['text'], 0, 25) . '...' ?></p>
-                </div>
-
-            <?php endforeach; ?>
-        </div>
-
-        <?php include_once './view/assets/nav.php'; ?>
-    </div>
-
+    <?php if (empty($this->data)) {
+        include_once './view/assets/discussions.php';
+    } else {
+        include_once './view/assets/disscussion.php';
+    }
+    ?>
 </div>
 
 <?php include_once './view/assets/footer.php'; ?>
